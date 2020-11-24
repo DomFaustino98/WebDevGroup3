0fb53aef57193e0dab20f1f2989e1dd347c27b46
AndrewNoonan <andrewnoonan3@gmail.com>
2020-11-24 13:28:29 -0500

    Server and json update

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // need to add server details
+
+//Local host
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+//to run: npm run dev
