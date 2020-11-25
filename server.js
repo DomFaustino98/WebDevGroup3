22ec8960059881f845cd01b1a93cf714bb0caaea
AndrewNoonan <andrewnoonan3@gmail.com>
2020-11-24 19:44:11 -0500

    updates

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // need to add server details
 
 //Local host
+
 const path = require('path');
 const express = require('express');
 
 const app = express();
 
 //Static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'WEBDEVGROUP3')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
+app.listen(3000, function () {
+    console.log("Server is running on localhost3000");
+});
 //to run: npm run dev
