274d2bdb4326946a091d40ee4e65adf7ab6c2aa4
Brian <bscheu1@students.towson.university>
2020-11-27 20:03:27 -0500

    Updates to express

--- nodetest/server.js
+++ nodetest/server.js
@@ -2,21 +2,28 @@ const express = require('express')
 const bodyParser= require('body-parser') 
 const path = require('path');
 const app = express(); 
+const MongoClient = require('mongodb').MongoClient  
+
+
+var uri = 'mongodb+srv://studentUser:iamstudent@cluster0.y9n1j.mongodb.net/movieDB?retryWrites=true&w=majority';
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(session({secret: "Shh, its a secret!"}));
+app.use(session({secret: "Your secret key"}));
 
-const MongoClient = require('mongodb').MongoClient  
-var uri = 'mongodb+srv://studentUser:iamstudent@cluster0.y9n1j.mongodb.net/movieDB?retryWrites=true&w=majority';
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+
+
 MongoClient.connect(uri, (err, database) => {
    // ... start the server 
 })
-
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect((err, database) => {
@@ -31,23 +38,19 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
 
-app.get('/index', (req, res) => {
-   var cursor = db.collection('User').find().toArray(function(err, result){
-    if (err) { return console.log(err) }
-    console.log('haanbhai', result)
-    res.render('index.ejs', {User: result})
-  })
-})
-
 app.post('/profiler/profilerProc.html', (req, res) => {
   var cursor = db.collection('User').findOne({id: req.body.id}, function(err, result){
     if(result){
-      res.render('profileMMU.ejs', {User: result})
+      var newUser = {id: req.body.id};
+      req.session.user = newUser;
+      res.render('profileMMU.ejs', {User: result, session: newUser})
     }
     else {
       db.collection('User').save(req.body, (err, result) => {
         if (err) return console.log(err)
-        res.redirect('/profiler/profileMMU.ejs')
+        var newUser = {id: req.body.id};
+        req.session.user = newUser;
+        res.render('profileMMU.ejs', {User: result})
       })
   }
 })
@@ -60,3 +63,47 @@ app.post('/profiler/editProfile.html', (req, res) => {
 app.post('/profile.html', (req, res) => {
 res.sendFile(__dirname + '/index.ejs')
 })
+
+app.get('/profileMMU', checkSignIn, function(req, res){
+  console.log("here")
+  res.render('profileMMU.ejs', {User: result})
+});
+
+app.post('/pageNameHere.html', (req, res) => {
+  console.log(req.body);
+})
+
+app.post('/profiler/EditProfileProc', (req, res) => {
+  if(req.session.user.id){
+    var cursor = db.collection('User').findOne({id: req.session.user.id}, function(err, result){
+      if(result){
+        var userUpdate = {id: req.session.user.id};
+        db.collection('User').update(userUpdate, {$set: {firstname: req.body.fname, lastname: req.body.lname, bio: req.body.bio, favmovie: req.body.favMovie, profilepic: req.body.profilePicURL}, }, function(err, res){
+          if (err) throw err;
+          console.log("successfully updated");
+          res.render('profileMMU.ejs', {User: result})
+        })
+  }
+})
+  }
+})
+
+app.get('/profiler/editProfile', (req, res) => {
+  if(req.session.user.id){
+    var cursor = db.collection('User').findOne({id: req.session.user.id}, function(err, result){
+    if (result){
+      res.render('editProfile.ejs', {User: result})
+    }
+  })
+}
+})
+
+function checkSignIn(req, res){
+  if(req.session.user){
+     next();     //If session exists, proceed to page
+  } else {
+     var err = new Error("Not logged in!");
+     console.log(req.session.user);
+     next(err);  //Error, trying to access unauthorized page!
+  }
+}
