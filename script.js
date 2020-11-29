9491bac256b2774d259d352fa3dca112acc859aa
AndrewNoonan <andrewnoonan3@gmail.com>
2020-11-29 18:55:43 -0500

    Small Changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
+const name = prompt("What is your name?")
 appendMessage('You joined')
 socket.emit('new-user', name)
 
